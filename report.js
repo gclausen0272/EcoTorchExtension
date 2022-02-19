fb2c316735ed3c55829e889e0ea5a02d3bb9987b
Cindy Chen <u6827101@anu.edu.au>
2022-02-20 00:10:11 +0800

    draft

--- report.js
+++ report.js
@@ -2,10 +2,22 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
+let suggestion = document.getElementById("suggestion");
+let reset = document.getElementById("reset")
+
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
+
+reset.addEventListener("click", async () => {
+  console.log("reset button")
+  window.location.reload(true);
+});
+
+// suggestion.addEventListener("click", async () => {
+
+// });
 
 // onClick={() => window.location.href='/'}
