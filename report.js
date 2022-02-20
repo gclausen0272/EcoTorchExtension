8768c7f367da24c3b8b6041df935298d60820cc4
Cindy Chen <u6827101@anu.edu.au>
2022-02-20 14:55:36 +0800

    draft

--- report.js
+++ report.js
@@ -1,24 +1,21 @@
-// var slider = document.getElementById("myRange");
-// var output = document.getElementById("demo");
-
-// // output.innerHTML = slider.value; // Display the default slider value
-
 // let suggestion = document.getElementById("suggestion");
 // let reset = document.getElementById("reset")
 
-// // data = '[{"emissions": "500", "duration": "30"}]'
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// var step = document.getElementById("new")
+// output.innerHTML = step.value;
+
+// step.oninput = function() {
+//   output.innerHTML = this.value;
+// }
+
 function load_data(){
   var mydata = JSON.parse(data);
   console.log("data coming")
   document.getElementById("emissions").innerHTML = mydata[0].emissions
   document.getElementById("duration").innerHTML = mydata[0].duration
 }
-
-
-// Update the current slider value (each time you drag the slider handle)
-// slider.oninput = function() {
-//   output.innerHTML = this.value;
-// }
 
 // reset.addEventListener("click", async () => {
 //   console.log("reset button")
