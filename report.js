6997ca7334db4a5704d33aa34fcd748e85fb5a8c
Cindy Chen <u6827101@anu.edu.au>
2022-02-20 16:40:58 +0800

    slider with marks

--- report.js
+++ report.js
@@ -12,9 +12,19 @@ elem.addEventListener("input", rangeValue);
 
 function load_data(){
   var mydata = JSON.parse(data);
-  console.log("data coming")
+  console.log(mydata)
   document.getElementById("emissions").innerHTML = mydata[0].emissions
   document.getElementById("duration").innerHTML = mydata[0].duration
+
+  document.getElementById("devA").innerHTML = mydata[0].devA
+  document.getElementById("devB").innerHTML = mydata[0].devB
+  document.getElementById("devC").innerHTML = mydata[0].devC
+
+  // var devData = mydata[0].devices
+  // console.log(devData)
+  // document.getElementById("devA").innerHTML = devData[0].devA
+  // document.getElementById("devB").innerHTML = devData[0].devB
+  // document.getElementById("devC").innerHTML = devData[0].devC
 }
 
 window.addEventListener('load', load_data);
